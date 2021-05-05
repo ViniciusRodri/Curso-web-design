df2d5bf1ecd1d590cb95fd384e2b576c23039c27
Vinicius Alves Rodrigues Pinto <60937919+ViniciusRodri@users.noreply.github.com>
2021-05-05 10:37:24 -0300

    Add files via upload

---
+++ web/js/script.js
@@ -0,0 +1,19 @@
+if(window.SimpleSlide){
+
+  new SimpleSlide({
+    slide: 'quote',
+    time: 5000, 
+    pauseOnHover: true, 
+  });
+
+  new SimpleSlide({
+    slide: "portifolio",
+    time: 5000,
+    pauseOnHover: true,
+    nav: true,
+  });
+}
+
+if (Window.SimpleAnime) {
+new SimpleAnime();
+}
